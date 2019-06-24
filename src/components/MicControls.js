9f39eb114e17c9c4a8aa06ae2b4bf446de244fb0
Jean-Luc Martin <jl.martin1990@gmail.com>
2019-06-23 21:44:48 -0400

    able to toggle recording button state

--- src/components/MicControls.js
+++ src/components/MicControls.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class MicControls extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {isRecording: false}
+
+        this.toggleRecording = () => {
+            if (!this.state.isRecording) {
+                this.setState({isRecording: true})
+            } else {
+                this.setState({isRecording: false})
+            }
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.props.name}
+                <h1>{this.props.name}</h1>
+                <button onClick={this.toggleRecording}>{this.state.isRecording ? 'Stop Recording' : 'Start Recording'}</button>
             </div>
         )
     }
