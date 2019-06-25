d4225beff61928ff4ea91e2f19804c4436805f7b
Jean-Luc Martin <jl.martin1990@gmail.com>
2019-06-24 22:16:50 -0400

    play all now plays serially

--- src/components/SnippetRecorder.js
+++ src/components/SnippetRecorder.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import forEachAsync from 'async-await-foreach'
 import MicCreator from './MicCreator'
 import MicControls from './MicControls'
+
+function playUntilEnd(audioElm) {
+    return new Promise(resolve => {
+        audioElm.addEventListener('ended', resolve)
+
+        audioElm.play()
+    })
+}
 
 class SnippetRecorder extends Component {
     constructor(props) {
         super(props)
-        this.state = {mics: []}
+        this.state = {
+            mics: [],
+            isPlayingAll: false
+        }
 
         this.onMicCreated = micName => {
             this.setState({
@@ -13,8 +25,12 @@ class SnippetRecorder extends Component {
             })
         }
 
-        this.playAll = () => {
-            this.state.mics.forEach(mic => mic.audioRef.current.audioRef.current.play())
+        this.playAll = async () => {
+            this.setState({isPlayingAll: true})
+
+            await forEachAsync(this.state.mics, mic => playUntilEnd(mic.audioRef.current.audioRef.current))
+
+            this.setState({isPlayingAll: false})
         }
     }
     
